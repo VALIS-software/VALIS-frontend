e176a651b54bbc293160f3cfea0e1fa83e9b874f
qyd <saviorbruce@gmail.com>
2018-02-11 19:18:37 -0800

    fix path in prep_gcloud.sh

--- tools/prep_gcloud.sh
+++ tools/prep_gcloud.sh
@@ -7,9 +7,9 @@ export CLOUDSDK_COMPUTE_ZONE="us-west1-b"
 
 
 echo $GCLOUD_SERVICE_KEY | base64 --decode -i > ${HOME}//gcloud-service-key.json
-sudo /google-cloud-sdk/bin/gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
-sudo /google-cloud-sdk/bin/gcloud config set project $PROJECT_ID
-sudo /google-cloud-sdk/bin/gcloud --quiet config set container/cluster $CLUSTER_NAME
-sudo /google-cloud-sdk/bin/gcloud config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
-sudo /google-cloud-sdk/bin/gcloud --quiet container clusters get-credentials $CLUSTER_NAME
+gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
+gcloud config set project $PROJECT_ID
+gcloud --quiet config set container/cluster $CLUSTER_NAME
+gcloud config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
+gcloud --quiet container clusters get-credentials $CLUSTER_NAME
 
