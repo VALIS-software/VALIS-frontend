84193048fc52f032e50da5aa2653bcf1df306fd6
George Corney <haxiomic@gmail.com>
2018-03-12 18:21:25 +0000

    Add chromosome indicator to x-axis

--- src/components/XAxis/XAxis.jsx
+++ src/components/XAxis/XAxis.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import Util from '../../helpers/util.js';
 import DropDownMenu from 'material-ui/DropDownMenu';
 import MenuItem from 'material-ui/MenuItem';
-import { CHROMOSOME_START_BASE_PAIRS } from '../../helpers/constants.js';
+import { CHROMOSOME_START_BASE_PAIRS, GENOME_LENGTH } from '../../helpers/constants.js';
 
 const _ = require('underscore');
 const d3 = require('d3');
@@ -52,6 +52,8 @@ class XAxis extends React.Component {
                   .ticks(Math.floor(width/TICK_SPACING_PIXELS))
                   .tickFormat(this.renderBasePair);
 
+    let indicatorText = '';
+
     if ((endBasePair - windowState.startBasePair) > SHOW_CHROMOSOMES_THRESHOLD) {
       const ticks = _.filter(CHROMOSOME_START_BASE_PAIRS, d => {
         return d >= windowState.startBasePair && d <= endBasePair;
@@ -61,9 +63,24 @@ class XAxis extends React.Component {
                 .tickSizeOuter(-10)
                 .tickValues(ticks)
                 .tickFormat(this.renderChromosome);
+    } else {
+      let centerBasePair = (windowState.startBasePair + endBasePair) * 0.5;
+      centerBasePair = Math.max(centerBasePair, 0);
+      centerBasePair = Math.min(centerBasePair, GENOME_LENGTH);
+
+      // test if chromosomes are visible
+      const outOfBounds = (endBasePair < 0) || (windowState.startBasePair > GENOME_LENGTH);
+      const centerChromosome = outOfBounds ? null : Util.chromosomeIndex(centerBasePair);
+
+      if (centerChromosome != null) {
+        indicatorText = 'chromosome ' + Util.chromosomeIndexToName(centerChromosome);
+      }
     }
 
     return (<div className="track-header-axis">
+      <div className="x-axis-indicator">
+        <div className="indicator-content">{indicatorText}</div>
+      </div>
       <svg className="x-axis-container">
         <g className="x-axis" ref={node => d3.select(node).call(xAxis)} />
       </svg>
