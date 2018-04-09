86228e6717f7428fb8d21344f3026bc4fa0010b5
George Corney <haxiomic@gmail.com>
2018-04-10 00:56:57 +0100

    Remove old comment

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,3 @@
-/*
-
-	Viewer
-	- All coordinates are set in DOM pixel units relative to the canvas (unless marked as otherwise)
-
-	- DOM element node
-		- Returns some sort of react object thingy : Contains a react component?
-		- Extends Object2D with render false
-		- applyTreeTransforms updates the dimensions of the react component
-		! How do we know when to re-render?
-			- We have some state field that has all the ReactNode components
-		! How do we know when positions have changed?
-			- Each frame we build the list of ReactNodes, comparing as we go
-
-	- Split App into App and Viewer
-	- Viewer height should be set by content / or fill rest of page height
-
-	- Define a single track
-	- Define a trackset / multiple track view
-	- Define panel - a tracket with a header
-	- Define viewer?, which is a set of panels with DOM column headers
-		- Should have access to track rows since they'll be draggable in the future
-	- Animate in new panels
-	- Scene graph mouse events
-*/
-
 import * as React from "react";
 import { Viewer } from "./Viewer";
 
