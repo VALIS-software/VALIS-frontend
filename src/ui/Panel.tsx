9a9fcab1541383d0ad57e5977c090d18b845ac80
George Corney <haxiomic@gmail.com>
2018-05-04 20:15:49 +0100

    Improve x-axis display

--- src/ui/Panel.tsx
+++ src/ui/Panel.tsx
@@ -17,6 +17,9 @@ const OpenSansRegular = require('./../font/OpenSans-Regular.msdf.bin');
 export class Panel extends Object2D {
 
     column: number;
+    maxRange: number = 1e10;
+    minRange: number = 1;
+
     readonly header: ReactObject;
     readonly xAxis: XAxis;
     readonly resizeHandle: Rect;
@@ -39,7 +42,7 @@ export class Panel extends Object2D {
     // view model state
     // viewport is designed to weight high precision to relatively small values (~millions) and lose precision for high values (~billions+)
     readonly x0: number = 0; // these should only be changed by setRange()
-    readonly x1: number = 1;
+    readonly x1: number = 1e6;
 
     constructor(
         readonly model: PanelDataModel,
@@ -63,7 +66,7 @@ export class Panel extends Object2D {
         this.add(this.header);
 
         // 1/2 spacing around the x-axis
-        this.xAxis = new XAxis(this.x0, this.x1, 16);
+        this.xAxis = new XAxis(this.x0, this.x1, 11);
         this.xAxis.h = this.xAxisHeight;
         this.xAxis.layoutY = -1;
         this.fillX(this.xAxis);
@@ -124,22 +127,35 @@ export class Panel extends Object2D {
             e.preventDefault();
             e.stopPropagation();
 
+            let zoomFactor = 1;
+
             // pinch zoom
             if (e.ctrlKey) {
-                let zoomFactor = 1 + e.wheelDeltaY * 0.01;
-                let zoomCenterF = e.fractionX;
-
-                let range = this.x1 - this.x0;
-
-                let d0 = range * zoomCenterF;
-                let d1 = range * (1 - zoomCenterF);
-                let p = d0 + this.x0;
-
-                let x0 = p - d0 * zoomFactor;
-                let x1 = p + d1 * zoomFactor;
-
-                this.setRange(x0, x1);
+                zoomFactor = 1 + e.wheelDeltaY * 0.01; // I'm assuming mac trackpad outputs change in %, @! needs research   
+            } else {
+                zoomFactor = 1 + e.wheelDeltaY * 0.01 * 0.15;
             }
+
+            let zoomCenterF = e.fractionX;
+
+            let range = this.x1 - this.x0;
+
+            // clamp zoomFactor to range limits
+            if (range * zoomFactor > this.maxRange) {
+                zoomFactor = this.maxRange / range;
+            }
+            if (range * zoomFactor < this.minRange) {
+                zoomFactor = this.minRange / range;
+            }
+
+            let d0 = range * zoomCenterF;
+            let d1 = range * (1 - zoomCenterF);
+            let p = d0 + this.x0;
+
+            let x0 = p - d0 * zoomFactor;
+            let x1 = p + d1 * zoomFactor;
+
+            this.setRange(x0, x1);
         });
 
         this.fillX(tile);
@@ -156,8 +172,13 @@ export class Panel extends Object2D {
     }
 
     setRange(x0: number, x1: number) {
+        // if range is not a finite number then default to 0 - 1
+        x0 = isFinite(x0) ? x0 : 0;
+        x1 = isFinite(x1) ? x1 : 0;
+
         (this.x0 as any) = x0;
         (this.x1 as any) = x1;
+
         this.xAxis.setRange(x0, x1);
 
         // @! for each tile, setRange
@@ -165,7 +186,6 @@ export class Panel extends Object2D {
     }
 
     protected onTileDragMove = (e: InteractionEvent) =>{
-        console.log('tile drag', e.fractionX);
         e.preventDefault();
         e.stopPropagation();
     }
@@ -186,10 +206,13 @@ export class Panel extends Object2D {
 
 class XAxis extends Object2D {
 
+    maxTextLength: number = 4;
+    maxMajorTicks: number = 10000; // failsafe to avoid rendering hangs
+
     set fontSizePx(v: number) {
         this._fontSizePx = v;
-        this.clearTextCache();
-        this.updateLabels();
+        this.labelCacheClear();
+        this.labelsNeedUpdate = true;
     }
     get fontSizePx() {
         return this._fontSizePx;
@@ -198,6 +221,8 @@ class XAxis extends Object2D {
     protected x0: number = 0;
     protected x1: number = 1;
     protected _fontSizePx: number;
+    protected labelsNeedUpdate: boolean;
+    protected lastComputedWidth: number;
 
     constructor(x0: number = 0, x1: number = 1, fontSizePx: number = 16, protected fontPath: string = OpenSansRegular) {
         super();
@@ -205,81 +230,232 @@ class XAxis extends Object2D {
         this.x0 = x0;
         this.x1 = x1;
         this._fontSizePx = fontSizePx;
-        this.updateLabels();
+        this.labelsNeedUpdate = true;
     }
 
     setRange(x0: number, x1: number) {
+        this.labelsNeedUpdate = this.labelsNeedUpdate || this.x0 !== x0 || this.x1 !== x1;
         this.x0 = x0;
         this.x1 = x1;
-        this.updateLabels();
+    }
+
+    applyTreeTransforms(root?: boolean) {
+        this.labelsNeedUpdate = this.labelsNeedUpdate || this.getComputedWidth() !== this.lastComputedWidth;
+
+        if (this.labelsNeedUpdate) {
+            this.updateLabels();
+        }
+
+        super.applyTreeTransforms(root);        
+    }
+
+    formatValue(x: number, maxLength: number) {
+        let str = x.toString();
+
+        if (str.length > maxLength) {
+            // if default print of string is too long, try to reduce it with a exponent symbol
+            let exp10 = this.log10(Math.abs(x));
+            let exp10Sign = this.sign(exp10);
+            let exp1000Int = Math.floor(Math.abs(exp10 / 3)) * exp10Sign;
+
+            let symbol = XAxis.siPrefixes[exp1000Int.toFixed(0)];
+
+            if (symbol === void 0) {
+                let exp10Int = Math.floor(Math.abs(exp10)) * exp10Sign;
+                symbol = exp10Int <= 3 ? '' : 'e' + exp10Int.toFixed(0);
+            }
+
+            let m = x / Math.pow(1000, exp1000Int);
+            let dp = maxLength - (Math.round(m).toString()).length - symbol.length;
+            let mStr = dp > 0 ? m.toFixed(dp) : Math.round(m).toString();
+            str = mStr + symbol;
+        }
+
+        return str;
     }
 
     // valid for stable (fontSize, fontPath)
-    protected _textCache: { [ str: string ]: {
-        node: Text,
+    protected _labelCache: {
+        [ key: string ]: {
+        label: Label,
         used: boolean,
     } } = {};
 
-    protected updateLabels() {
-        let ticks = 100; // @! quick hack
-        let range = this.x1 - this.x0;
-        let dp = 1;
+    protected labelCacheClear() {
+        this._labelCache = {};
+    }
 
-        // @! align with each 0.5
-        // x0 to nearest 0.5
-        // x = 0.142
-        // Math.ceil((x * 2) /2;
-        let n = 2;
+    protected labelCacheGet(value: number, str: string) {
+        let key = value.toString() + '_' + str;
 
-        let s = range / (ticks - 1);
+        let entry = this._labelCache[key];
 
-        let start0 = Math.ceil(this.x0 * n) / n;
+        if (entry === void 0) {
+            console.log('new Text', str);
+            let label = new Label(this.fontPath, str, this.fontSizePx);
+            label.layoutParentY = 1;
+            this.add(label);
 
-        // reset 'used' flag in cache
-        for (let str in this._textCache) {
-            this._textCache[str].used = false;
-        }
-
-        for (let i = 0; i < ticks; i++) {
-            let v = start0 + i * (1/n);
-
-            if (v >  this.x1) break; // @! quick hack
-
-            let m = Math.pow(10, dp);
-            let str = (Math.round(v * m) / m).toString();
-
-            let entry = this._textCache[str];
-
-            if (entry === void 0) {
-                let text = new Text(this.fontPath, str, this._fontSizePx);
-                text.layoutX = -0.5;
-                text.layoutY = -1;
-                text.layoutParentY = 1;
-                this.add(text);
-
-                entry = this._textCache[str] = {
-                    node: text,
-                    used: true,
-                }
+            entry = this._labelCache[key] = {
+                label: label,
+                used: true,
             }
-
-            entry.used = true;
-
-            entry.node.layoutParentX = (v - this.x0)/range;
         }
 
-        // remove unused labels
-        for (let str in this._textCache) {
-            let entry = this._textCache[str];
+        entry.used = true;
+
+        return entry;
+    }
+
+    protected labelCacheMarkAllUnused() {
+        // reset 'used' flag in cache
+        for (let key in this._labelCache) {
+            this._labelCache[key].used = false;
+        }
+    }
+
+    protected labelCacheDeleteUnused() {
+        for (let key in this._labelCache) {
+            let entry = this._labelCache[key];
             if (!entry.used) {
-                this.remove(entry.node);
-                delete this._textCache[str];
+                entry.label.releaseGPUResources();
+                this.remove(entry.label);
+                delete this._labelCache[key];
             }
         }
     }
 
-    protected clearTextCache() {
-        this._textCache = {};
+    protected updateLabels() {
+        this.labelCacheMarkAllUnused();
+
+        // guard case where we cannot display anything
+        let range = this.x1 - this.x0;
+        if (range === 0) {
+            this.labelCacheDeleteUnused();
+            return;
+        }
+
+        const tickSpacingPx = 80 * 2;
+        const rangeWidthPx = this.getComputedWidth(); // @! synchronization issues
+        const tickRatio = tickSpacingPx / rangeWidthPx;
+        const snap = 5;
+
+        // @! problem is we're dealing in absolute space too much
+        // we should convert to absolute space only when displaying text
+
+        // let t0x = tickRatio * range; // x-space location of first tick after 0
+
+
+
+        let n = this.log2(tickRatio / snap) + this.log2(range);
+
+        let lMajor = Math.ceil(n);
+        let lMinor = Math.floor(n);
+        let a = n - lMinor; // current mix between lMinor and lMajor
+
+        let transitionSharpness = 5; // from 0 to Infinity
+        let minorAlpha = Math.pow((1 - a), transitionSharpness); // exponent is used to sharpen the transition
+
+        let xMajorSpacing = (snap * Math.pow(2, lMajor)); // cannot use bitwise arithmetic because we're limited to 32 bits in js
+        let xMinorSpacing = (snap * Math.pow(2, lMinor)) * 2;
+
+        let firstDisplayTick = Math.floor(this.x0 / xMajorSpacing);
+        let lastDisplayTick = Math.ceil(this.x1 / xMajorSpacing);
+
+        let ticksRemaining = this.maxMajorTicks;
+        for (let i = firstDisplayTick; i <= lastDisplayTick && ticksRemaining > 0; i++) {
+            ticksRemaining--;
+
+            let xMinor = xMinorSpacing * (i + 0.5);
+            let xMajor = xMajorSpacing * i;
+
+            let minorParentX = (xMinor - this.x0) / range;
+            if (minorParentX >= 0 && minorParentX <= 1) { // @! temporary simple bounds clipping
+                let textMinor = this.labelCacheGet(xMinor, this.formatValue(xMinor, this.maxTextLength));
+                textMinor.label.layoutParentX = minorParentX;
+                textMinor.label.setColor(0, 0, 0, minorAlpha);
+            }
+
+            let majorParentX = (xMajor - this.x0) / range;
+            if (majorParentX >= 0 && majorParentX <= 1) {
+                let textMajor = this.labelCacheGet(xMajor, this.formatValue(xMajor, this.maxTextLength));
+                textMajor.label.layoutParentX = majorParentX;
+                textMajor.label.setColor(0, 0, 0, 1);
+            }
+        }
+
+
+        this.labelCacheDeleteUnused();
+        this.labelsNeedUpdate = false;
+        this.lastComputedWidth = this.getComputedWidth();
+    }
+
+    protected log2(x: number) {
+        return Math.log(x) / Math.LN2;
+    }
+
+    protected log10(x: number) {
+        return Math.log(x) / Math.LN10;
+    }
+
+    protected sign(x: number) {
+        return (((x > 0) as any) - ((x < 0) as any)) || +x;
+    }
+
+    static siPrefixes: { [key: string]: string } = {
+        '8': 'Y', // yotta
+        '7': 'Z', // zetta
+        '6': 'E', // exa
+        '5': 'P', // peta
+        '4': 'T', // tera
+        '3': 'G', // giga
+        '2': 'M', // mega
+        '1': 'k', // kilo
+        '-1': 'm', // milli
+        '-2': 'µ', // micro
+        '-3': 'n', // nano
+        '-4': 'p', // pico
+        '-5': 'f', // femto
+        '-6': 'a', // atto
+        '-7': 'z', // zepto
+        '-8': 'y', // yocto
+    };
+
+}
+
+class Label extends Object2D {
+
+    text: Text;
+    tick: Rect;
+
+    constructor(fontPath: string, string: string, fontSizePx: number) {
+        super();
+        let tickHeightPx = 5;
+        let tickWidthPx = 1;
+
+        this.text = new Text(fontPath, string, fontSizePx);
+        this.text.layoutX = -0.5;
+        this.text.layoutY = -1;
+        this.text.y = -tickHeightPx - 3;
+        this.add(this.text);
+
+        this.tick = new Rect(tickWidthPx, tickHeightPx);
+        this.tick.layoutX = -0.5;
+        this.tick.layoutY = -1;
+        this.add(this.tick);
+
+        this.render = false;
+        this.setColor(0, 0, 0, 1);
+    }
+
+    setColor(r: number, g: number, b: number, a: number) {
+        this.text.color.set([r, g, b, a]);
+        this.tick.color.set([r, g, b, a * 0.5]);
+    }
+
+    releaseGPUResources() {
+        this.text.releaseGPUResources();
+        this.tick.releaseGPUResources();
     }
 
 }
