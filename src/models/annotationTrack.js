24991efa88fb424f1d76b9e3bcd2f43442c65bfb
qyd <saviorbruce@gmail.com>
2018-05-10 17:34:06 -0700

    fix contig coord to global coord conversion

--- src/models/annotationTrack.js
+++ src/models/annotationTrack.js
@@ -1,7 +1,7 @@
 
 import Util from '../helpers/util.js';
 import { Tile, TileCache, LinearCacheSampler, FixedCacheSampler } from '../helpers/cache.js';
-import { GENOME_LENGTH } from '../helpers/constants.js';
+import { GENOME_LENGTH, CHROMOSOME_START_BASE_PAIRS } from '../helpers/constants.js';
 import Track, { TRACK_EVENT_LOADING } from './track.js';
 
 class AnnotationTrack extends Track {
@@ -46,6 +46,7 @@ class AnnotationTrack extends Track {
     // Translate to chromosome centric coordinate:
     const range = Util.chromosomeRelativeRange(start, end);
     const contig = 'chr' + Util.chromosomeIndexToName(range.chromosomeIndex);
+    const contigStart = CHROMOSOME_START_BASE_PAIRS[range.chromosomeIndex];
 
     this.notifyListeners(TRACK_EVENT_LOADING, true);
     const promise = this.api.getAnnotationData(this.annotationId, contig, range.start, range.end, samplingRate, trackHeightPx, this.query);
@@ -55,13 +56,12 @@ class AnnotationTrack extends Track {
       const totalCount = data.data.countInRange;
       this.notifyListeners(TRACK_EVENT_LOADING, false);
       // convert contig coordinate back to global coordinate
-      // This is commented out because it's not working well
-      // rawData.forEach(r => {
-      //   r.startBp = Util.chromosomeRelativeToUniversalBasePair(range.chromosomeIndex, r.startBp);
-      //   r.endBp = Util.chromosomeRelativeToUniversalBasePair(range.chromosomeIndex, r.endBp);
-      // });
-      const startBp = Util.chromosomeRelativeToUniversalBasePair(range.chromosomeIndex, result.startBp);
-      const endBp = Util.chromosomeRelativeToUniversalBasePair(range.chromosomeIndex, result.endBp);
+      rawData.forEach(r => {
+        r.startBp += contigStart;
+        r.endBp += contigStart;
+      });
+      const startBp = result.startBp + contigStart;
+      const endBp = result.endBp + contigStart;
       return new Tile([start, end], [startBp, endBp], result.samplingRate, result.trackHeightPx, rawData, totalCount);
     }, failure => {
       this.notifyListeners(TRACK_EVENT_LOADING, false);
