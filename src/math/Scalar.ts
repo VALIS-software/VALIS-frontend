6cd065c31ede5e3458b4c4c7ec51158e33db66d6
George Corney <haxiomic@gmail.com>
2018-06-01 16:00:12 +0100

    Docs and handle non-finite numbers

--- src/math/Scalar.ts
+++ src/math/Scalar.ts
@@ -16,17 +16,33 @@ export class Scalar {
         return Math.min(Math.max(x, min), max);
     }
 
-    // convert a number to a fixed point representation by truncating instead of rounding
+    /**
+     * Convert a number to a fixed point representation by truncating instead of rounding
+     * 
+     * For example:
+     *  - `toFixedTrunc(999.996, 2) => "999.99"`
+     *  - `toFixedTrunc(999.996, 5) => "999.99600"`
+     *  - `toFixedTrunc(999.996, 0) => "999"`
+     */
     static toFixedTrunc(x: number, decimalPoints: number): string {
         let str = x.toString();
         let pattern = /([\d+-]+)(\.(\d*))?(.*)/;
         let result = pattern.exec(str);
+
+        // if it doesn't match a number pattern then return the .toString() result
+        // this catches cases such as Infinity or NaN
+        if (result == null) {
+            return str;
+        }
+
+        // extract pattern parts
         let integerPart = result[1];
         let fractionalPart = result[3];
         let trailingCharacters = result[4];
 
         // truncate fractional part to show just 'decimalPoints' numbers
         let fractionString = (fractionalPart || '').substring(0, decimalPoints);
+
         // right-pad with 0s
         for (let i = fractionString.length; i < decimalPoints; i++) {
             fractionString += '0';
