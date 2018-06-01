91f33350dcd57d1fe31f2a29dc8218fc83092daa
George Corney <haxiomic@gmail.com>
2018-06-01 18:59:09 +0100

    Minor reorganisation

--- src/math/Scalar.ts
+++ src/math/Scalar.ts
@@ -1,5 +1,11 @@
 export class Scalar {
 
+    static clamp(x: number, min: number, max: number) {
+        return Math.min(Math.max(x, min), max);
+    }
+
+    // polyfill for ECMAScript 2015 Math methods
+
     static log2(x: number) {
         return Math.log(x) * Math.LOG2E;
     }
@@ -10,46 +16,6 @@ export class Scalar {
 
     static sign(x: number) {
         return (((x > 0) as any) - ((x < 0) as any)) || +x;
-    }
-
-    static clamp(x: number, min: number, max: number) {
-        return Math.min(Math.max(x, min), max);
-    }
-
-    /**
-     * Convert a number to a fixed point representation by truncating instead of rounding
-     * 
-     * For example:
-     *  - `toFixedTrunc(999.996, 2) => "999.99"`
-     *  - `toFixedTrunc(999.996, 5) => "999.99600"`
-     *  - `toFixedTrunc(999.996, 0) => "999"`
-     */
-    static toFixedTrunc(x: number, decimalPoints: number): string {
-        let str = x.toString();
-        let pattern = /([\d+-]+)(\.(\d*))?(.*)/;
-        let result = pattern.exec(str);
-
-        // if it doesn't match a number pattern then return the .toString() result
-        // this catches cases such as Infinity or NaN
-        if (result == null) {
-            return str;
-        }
-
-        // extract pattern parts
-        let integerPart = result[1];
-        let fractionalPart = result[3];
-        let trailingCharacters = result[4];
-
-        // truncate fractional part to show just 'decimalPoints' numbers
-        let fractionString = (fractionalPart || '').substring(0, decimalPoints);
-
-        // right-pad with 0s
-        for (let i = fractionString.length; i < decimalPoints; i++) {
-            fractionString += '0';
-        }
-
-        // recompose number
-        return integerPart + (fractionString.length > 0 ? '.' + fractionString : '') + trailingCharacters;
     }
 
 }
--- src/ui/XAxis.ts
+++ src/ui/XAxis.ts
@@ -78,7 +78,7 @@ export class XAxis extends Object2D {
             let reducedXIntStr = Math.floor(reducedX).toFixed(0);
             let dp = maxLength - reducedXIntStr.length - symbol.length;
 
-            let numString = Scalar.toFixedTrunc(reducedX, Math.max(dp, 0));
+            let numString = XAxis.toFixedTrunc(reducedX, Math.max(dp, 0));
 
             str = numString + symbol;
         }
@@ -173,6 +173,42 @@ export class XAxis extends Object2D {
     protected deleteLabel = (label: Label) => {
         label.releaseGPUResources();
         this.remove(label);
+    }
+
+    /**
+     * Convert a number to a fixed point representation by truncating instead of rounding
+     * 
+     * For example:
+     *  - `toFixedTrunc(999.996, 2) => "999.99"`
+     *  - `toFixedTrunc(999.996, 5) => "999.99600"`
+     *  - `toFixedTrunc(999.996, 0) => "999"`
+     */
+    static toFixedTrunc(x: number, decimalPoints: number): string {
+        let str = x.toString();
+        let pattern = /([\d+-]+)(\.(\d*))?(.*)/;
+        let result = pattern.exec(str);
+
+        // if it doesn't match a number pattern then return the .toString() result
+        // this catches cases such as Infinity or NaN
+        if (result == null) {
+            return str;
+        }
+
+        // extract pattern parts
+        let integerPart = result[1];
+        let fractionalPart = result[3];
+        let trailingCharacters = result[4];
+
+        // truncate fractional part to show just 'decimalPoints' numbers
+        let fractionString = (fractionalPart || '').substring(0, decimalPoints);
+
+        // right-pad with 0s
+        for (let i = fractionString.length; i < decimalPoints; i++) {
+            fractionString += '0';
+        }
+
+        // recompose number
+        return integerPart + (fractionString.length > 0 ? '.' + fractionString : '') + trailingCharacters;
     }
 
     static siPrefixes: { [key: string]: string } = {
