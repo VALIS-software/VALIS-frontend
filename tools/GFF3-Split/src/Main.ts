46cc5936e855e390575310ea809f491b04c14ea6
George Corney <haxiomic@gmail.com>
2018-06-13 18:16:25 +0100

    GFF3 split tool

---
+++ tools/GFF3-Split/src/Main.ts
@@ -0,0 +1,98 @@
+/**
+ * http://www.sequenceontology.org/browser/current_svn
+ * 
+ * - On genes vs transcripts https://www.biostars.org/p/244850/
+ * - On strands http://www.sci.sdsu.edu/~smaloy/MicrobialGenetics/topics/chroms-genes-prots/temp-strand.html
+ * 
+ * - Gene
+ *  `A gene is a locus that produces a set of similar and functionally-related transcripts`
+ *  `Includes all of the sequence elements necessary to encode a functional transcript. A gene may include regulatory regions, transcribed regions and/or other functional sequence regions.`
+ * 
+ *     - Transcript
+ *       `a given possible RNA transcript associated with a GeneRegion`
+ * 
+ * Assumptions
+ *  - A 'gene' by default is a protein-coding gene
+ */
+
+import * as fs from 'fs';
+import AnnotationTileset from '../../../lib/sirius/AnnotationTileset';
+import Gff3Parser, { Feature } from '../../../lib/gff3/Gff3Parser';
+import { Terminal } from './Terminal';
+
+let outputDirectory = '../../data/chromosome1/annotation';
+let inputPath = 'data/Homo_sapiens.GRCh38.92.chromosome.1.gff3';
+
+let unknownFeatureTypes: { [key: string]: number }  = {};
+
+let tileset = new AnnotationTileset(
+	1 << 20,
+	(f) => {
+		unknownFeatureTypes[f.type] = (unknownFeatureTypes[f.type] || 0) + 1;
+	},
+	Terminal.error,
+);
+
+let stat = fs.statSync(inputPath);
+
+let stream = fs.createReadStream(inputPath, {
+	encoding: 'utf8',
+	autoClose: true,
+});
+
+const progressDelta_ms = 1000/60;
+let lastProgressTime_ms = -Infinity;
+
+let parser = new Gff3Parser({
+
+	onFeatureComplete: (feature) => {
+		
+		// log progress
+		let hrtime = process.hrtime();
+		let t = hrtime[0] * 1000 + hrtime[1]/1000000;
+		let dt = t - lastProgressTime_ms;
+		if (dt > progressDelta_ms) {
+			Terminal.clearLine();
+			Terminal.log(`Processing <b>${Math.round(100 * stream.bytesRead / stat.size)}%</b>`);
+			Terminal.cursorUp();
+			lastProgressTime_ms = t;
+		}
+
+		tileset.addTopLevelFeature(feature);
+	},
+
+	onError: Terminal.error,
+	onComment: (c) => Terminal.log(`<dim><i>${c}<//>`),
+
+	onComplete: (gff3) => {
+		Terminal.clearLine();
+
+		Terminal.warn('Unknown features:', unknownFeatureTypes);
+
+		if (fs.existsSync(outputDirectory)) {
+			for (let filename of fs.readdirSync(outputDirectory)) {
+				fs.unlinkSync(outputDirectory + '/' + filename);
+			}
+			fs.rmdirSync(outputDirectory)
+		}
+
+		if (!fs.existsSync(outputDirectory)) {
+			fs.mkdirSync(outputDirectory);
+		}
+
+		let nSavedFiles = 0;
+		for (let tile of tileset.tiles) {
+			let filename = `${tile.startIndex.toFixed(0)},${tile.span.toFixed(0)}`;
+			let filePath = `${outputDirectory}/${filename}.json`;
+			fs.writeFileSync(filePath, JSON.stringify(tile.content));
+			nSavedFiles++;
+		}
+
+		Terminal.success(`Saved <b>${nSavedFiles}</b> files into <b>${outputDirectory}</b>`);
+	}
+});
+
+stream.on('data', parser.parseChunk);
+stream.on('close', parser.end);
+
+Terminal.log(`Reading <b>${inputPath}</b>`);
