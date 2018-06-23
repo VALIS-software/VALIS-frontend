a69084a8ea014e0f2a323ff54be5c0c4ff331c55
Salik Syed <saliksyed@Saliks-MacBook-Pro.local>
2018-06-23 15:41:23 -0700

    remove dead code #unreviewed-commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -36,31 +36,6 @@ class Header extends React.Component {
     };
   }
 
-  onNewRequest = (chosen, index) => {
-    // treat as trait if the text is not from autocomplete
-    let text = chosen;
-    let value = 0;
-    if (index >= 0) {
-      text = chosen.textKey;
-      value = chosen.valueKey;
-    }
-    const builder = new QueryBuilder();
-    if (value === 0) {
-      builder.newInfoQuery();
-      builder.filterType('trait');
-      builder.searchText(text);
-      builder.setLimit(150);
-    } else if (value === 1) {
-      builder.newGenomeQuery();
-      builder.filterType('gene');
-      builder.filterName(text);
-      builder.setLimit(150);
-    }
-    const query = builder.build();
-    const view = (<SearchResultsView text={text} query={query} viewModel={this.props.viewModel} appModel={this.props.model} />);
-    this.props.viewModel.pushView('Search Results', query, view);
-  }
-
   addDatasetBrowser = () => {
     const view = (<DatasetSelector viewModel={this.props.viewModel} appModel={this.props.model} />);
     this.props.viewModel.pushView('Select Dataset', null, view);
