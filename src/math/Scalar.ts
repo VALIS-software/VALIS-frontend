a2fadd865c0340f57690866643ac102686fe5689
George Corney <haxiomic@gmail.com>
2018-06-25 17:25:34 +0100

    add smoothstep, rename linStep

--- src/math/Scalar.ts
+++ src/math/Scalar.ts
@@ -7,8 +7,18 @@ export class Scalar {
     /**
      * Return 0 at x <= edge0, return 1 for x >= edge1 and linearly interpolate in-between
      */
-    static linStep(edge0: number, edge1: number, x: number) {
+    static linstep(edge0: number, edge1: number, x: number) {
         return Scalar.clamp((x - edge0) / (edge1 - edge0), 0, 1);
+    }
+
+    /**
+     * Replicate GLSL smoothstep
+     * - Return 0 for x <= edge0, 1 for x >= edge 1, and interpolate via 3x^2 - 2x^3 in-between
+     * - Gradient is 0 at x = 0 and x = 1
+     */
+    static smoothstep(edge0: number, edge1: number, x: number) {
+        x = Scalar.clamp((x - edge0) / (edge1 - edge0), 0.0, 1.0);
+        return x * x * (3 - 2 * x);
     }
 
     // polyfill for ECMAScript 2015 Math methods
