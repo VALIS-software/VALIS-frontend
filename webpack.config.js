5b44f95fe8f21c00c643fa47b160099cfa54540a
George Corney <haxiomic@gmail.com>
2018-07-03 17:29:00 +0100

    Add deploy script

--- webpack.config.js
+++ webpack.config.js
@@ -1,81 +1,122 @@
 const path = require('path');
 const DefinePlugin = require("webpack").DefinePlugin;
 
-module.exports = (env) => ({
-	context: path.resolve(__dirname, "src"),
-	entry: "./index.tsx",
+const outputDirectory = `${__dirname}/dist`;
 
-	output: {
-		filename: "bundle.js",
-		path: __dirname + "/dist"
-	},
+module.exports = (env) => {
+	const config = {
+		context: path.resolve(__dirname, "src"),
+		entry: "./index.tsx",
 
-	// Enable sourcemaps for debugging webpack's output.
-	devtool: "source-map",
+		mode: (env.production || env.deploy) ? "production" : "development",
 
-	resolve: {
-		// Add '.ts' and '.tsx' as resolvable extensions.
-		modules: ['../node_modules', '../lib'],
-		extensions: [".ts", ".tsx", ".js", ".jsx", ".json"]
-	},
+		output: {
+			filename: "bundle.js",
+			path: outputDirectory
+		},
 
-	module: {
-		rules: [
-			// Enable importing scss files (and translate to CSS)
-			{
-				test: /\.(scss|css)$/,
-				use: [{
-					loader: "style-loader" // creates style nodes from JS strings
-				}, {
-					loader: "css-loader" // translates CSS into CommonJS
-				}, {
-					loader: "sass-loader" // compiles Sass to CSS
-				}]
-			},
+		// Enable sourcemaps for debugging webpack's output.
+		devtool: "source-map",
 
-			// This enables us to copy index.html into dist/
-			{
-				test: /\.html/,
-				type: 'javascript/auto',
-				use: [{
-					loader: 'file-loader',
-					options: { name: '[name].[ext]' },
-				}],
-			},
+		resolve: {
+			// Add '.ts' and '.tsx' as resolvable extensions.
+			modules: ['../node_modules', '../lib'],
+			extensions: [".ts", ".tsx", ".js", ".jsx", ".json"]
+		},
 
-			// Copy assets into dist/static/
-			{
-				test: /\.json|\.png|\.bin/,
-				type: 'javascript/auto',
-				use: [{
-					loader: 'file-loader',
-					options: { name: 'static/[path][name].[ext]' },
-				}],
-			},
+		module: {
+			rules: [
+				// Enable importing scss files (and translate to CSS)
+				{
+					test: /\.(scss|css)$/,
+					use: [{
+						loader: "style-loader" // creates style nodes from JS strings
+					}, {
+						loader: "css-loader" // translates CSS into CommonJS
+					}, {
+						loader: "sass-loader" // compiles Sass to CSS
+					}]
+				},
 
-			// All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-			{
-				test: /\.(ts|tsx|js|jsx)$/,
-				loader: "ts-loader",
-				exclude: [path.resolve(__dirname, "node_modules")],
-			},
+				// This enables us to copy index.html into dist/
+				{
+					test: /\.html/,
+					type: 'javascript/auto',
+					use: [{
+						loader: 'file-loader',
+						options: { name: '[name].[ext]' },
+					}],
+				},
 
-			// All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-			{ enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
-		]
-	},
+				// Copy assets into dist/static/
+				{
+					test: /\.json|\.png|\.bin/,
+					type: 'javascript/auto',
+					use: [{
+						loader: 'file-loader',
+						options: { name: 'static/[path][name].[ext]' },
+					}],
+				},
 
-	plugins: [
-		// pass --env to javascript build via process.env
-		new DefinePlugin({ "process.env": JSON.stringify(env) })
-	],
+				// All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+				{
+					test: /\.(ts|tsx|js|jsx)$/,
+					loader: "ts-loader",
+					exclude: [path.resolve(__dirname, "node_modules")],
+				},
 
-	// When importing a module whose path matches one of the following, just
-	// assume a corresponding global variable exists and use that instead.
-	// This is important because it allows us to avoid bundling all of our
-	// dependencies, which allows browsers to cache those libraries between builds.
-	externals: {
-		"react": "React",
-		"react-dom": "ReactDOM"
-	},
-});
+				// All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+				{ enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+			]
+		},
+
+		plugins: [
+			// pass --env to javascript build via process.env
+			new DefinePlugin({ "process.env": JSON.stringify(env) })
+		],
+
+		// When importing a module whose path matches one of the following, just
+		// assume a corresponding global variable exists and use that instead.
+		// This is important because it allows us to avoid bundling all of our
+		// dependencies, which allows browsers to cache those libraries between builds.
+		externals: {
+			"react": "React",
+			"react-dom": "ReactDOM"
+		},
+	}
+
+	// if in deploy mode, add deployment plugins
+	if (env.deploy) {
+		console.log(`Deployment Mode`);
+
+		const WebpackGoogleCloudStoragePlugin = require("webpack-google-cloud-storage-plugin");
+		const ZipPlugin = require("zip-webpack-plugin");
+
+		config.plugins = config.plugins.concat([
+			
+			// Zip up ./dist/
+			// (source path defaults to webpack output path)
+			new ZipPlugin({ path: outputDirectory, filename: "dist.zip" }),
+
+			new WebpackGoogleCloudStoragePlugin({
+				directory: outputDirectory,
+				include: ["dist.zip"],
+				exclude: [],
+				storageOptions: {
+					projectId: "valis-194104",
+					keyFilename: path.join(
+						process.env.HOME,
+						"gcloud-service-key.json" // This shouldn't be included in the repository!
+					)
+				},
+				uploadOptions: {
+					bucketName: "valis-front-dev",
+					gzip: false
+				}
+			})
+
+		]);
+	}
+
+	return config;
+};
