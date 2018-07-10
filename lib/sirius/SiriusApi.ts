4967c050ccfeee0d0ac294c58fc89b2fd8b2b430
George Corney <haxiomic@gmail.com>
2018-07-10 22:12:23 +0100

    Remove sample sequence loading

--- lib/sirius/SiriusApi.ts
+++ lib/sirius/SiriusApi.ts
@@ -39,10 +39,6 @@ export class SiriusApi {
         },
         indicesPerBase: number,
     }> {
-        if (sequenceId === '__SAMPLE__') {
-            return this.sample_LoadACGTSubSequence(sequenceId, lodLevel, lodStartBaseIndex, lodSpan);
-        }
-
         let samplingDensity = (1 << lodLevel);
         let startBasePair = samplingDensity * lodStartBaseIndex + 1;
         let spanBasePair = lodSpan * samplingDensity;
@@ -128,47 +124,6 @@ export class SiriusApi {
         });
     }
 
-    static sample_LoadACGTSubSequence(
-        sequenceId: string,
-        lodLevel: number,
-        lodStartBaseIndex: number,
-        lodSpan: number,
-    ): Promise<{
-        array: Uint8Array,
-        sequenceMinMax: {
-            min: number,
-            max: number,
-        },
-        indicesPerBase: number, 
-    }> {
-        let binPath = `data/chromosome1/dna/${lodLevel}.bin`;
-        let minMaxPath = binPath + '.minmax';
-
-        // @! data format may change for certain LODs in the future
-        let elementSize_bits = 8;
-
-        let dataPromise = SiriusApi.loadArray(binPath, elementSize_bits, lodStartBaseIndex * 4, lodSpan * 4, ArrayFormat.UInt8);
-
-        let minMaxPromise = SiriusApi.minMaxCache[minMaxPath];
-        
-        if (minMaxPromise === undefined) {
-            minMaxPromise = axios.get(minMaxPath, { responseType: 'json' }).then((a) => {
-                let minMax: { min: number, max: number } = a.data;
-                return minMax;
-            });
-            SiriusApi.minMaxCache[minMaxPath] = minMaxPromise;            
-        }
-        
-        return Promise.all([dataPromise, minMaxPromise])
-            .then((a) => {
-                return {
-                    array: a[0],
-                    sequenceMinMax: a[1],
-                    indicesPerBase: 4,
-                }
-            });
-    }
-
     private static parseSiriusBinaryResponse(arraybuffer: ArrayBuffer) {
         let byteView = new Uint8Array(arraybuffer);
 
@@ -187,77 +142,6 @@ export class SiriusApi {
 
         let payloadBytes = arraybuffer.slice(nullByteIndex + 1);
         return payloadBytes;
-    }
-
-    private static loadArray<T extends keyof ArrayFormatMap>(
-        path: string,
-        elementSize_bits: number,
-        elementIndex: number,
-        nElements: number,
-        targetFormat: T,
-        cancelToken?: CancelToken,
-    ): Promise<ArrayFormatMap[T]> {
-        let element0_bits = elementIndex * elementSize_bits;
-        let byte0 = Math.floor(element0_bits / 8);
-        let nBytes = Math.ceil(nElements * elementSize_bits / 8);
-        let offset_bits = element0_bits % 8;
-
-        // determine byte range from dataFormat
-        let byteRange = {
-            start: byte0,
-            end: byte0 + nBytes - 1,
-        };
-
-        return axios({
-            method: 'get',
-            url: path,
-            responseType: 'arraybuffer',
-            headers: {
-                'Range': `bytes=${byteRange.start.toFixed(0)}-${byteRange.end.toFixed(0)}`,
-                'Cache-Control': 'no-cache', // @! work around chrome bug
-            },
-            cancelToken: cancelToken
-        }).then((a) => {
-            let unpackingRequired = !((targetFormat === ArrayFormat.UInt8) && (elementSize_bits === 8));
-
-            if (unpackingRequired) {
-
-                let bytes: Uint8Array = new Uint8Array(a.data);
-
-                // allocate output
-                let outputArray: ArrayFormatMap[T];
-
-                switch (targetFormat) {
-                    case ArrayFormat.Float32:
-                        outputArray = new Float32Array(nElements);
-                        break;
-                    case ArrayFormat.UInt8:
-                        outputArray = new Uint8Array(nElements);
-                        break;
-                }
-
-                for (let element = 0; element < nElements; element++) {
-                    let bitIndex0 = element * elementSize_bits + offset_bits;
-                    let bitOffset = bitIndex0 % 8;
-                    let byteIndex0 = Math.floor(bitIndex0 / 8);
-                    /*
-                    let uint32 = composeUInt32(
-                        bytes[byteIndex0 + 0],
-                        bytes[byteIndex0 + 1],
-                        bytes[byteIndex0 + 2],
-                        bytes[byteIndex0 + 3]
-                    );
-
-                    outputArray[element] = uint32 & mask32(offset, length) <bit shift>;
-                    */
-                }
-
-                throw `Unpacking data not yet supported`;
-
-            } else {
-                return new Uint8Array(a.data);
-            }
-        });
     }
 
 }
