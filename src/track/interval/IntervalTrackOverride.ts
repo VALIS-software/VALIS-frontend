9e72657fdc9cfed2f2b23317c52788a574bdbbaf
George Corney <haxiomic@gmail.com>
2018-10-31 19:38:05 +0000

    Support recent changes to genome-browser, including interval model refactor

---
+++ src/track/interval/IntervalTrackOverride.ts
@@ -0,0 +1,24 @@
+import { IntervalTrack, BlendMode, Tile } from "genome-browser";
+import { IntervalTrackModelOverride } from "./IntervalTrackModelOverride";
+
+/**
+ * Override interval track to add a 'blendEnabled' flag
+ */
+export class IntervalTrackOverride extends IntervalTrack {
+
+    constructor(model: IntervalTrackModelOverride) {
+        super(model);
+    }
+
+    protected displayTileNode(tile: Tile<any>, z: number, x0: number, span: number, continuousLodLevel: number) {
+        let node = super.displayTileNode(tile, z, x0, span, continuousLodLevel);
+
+        if (this.model.blendEnabled === false) {
+            node.opacity = 1;
+            node.blendMode = BlendMode.NONE;
+        }
+
+        return node;
+    }
+
+}
