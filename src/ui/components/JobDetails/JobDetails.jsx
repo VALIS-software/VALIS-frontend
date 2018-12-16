b9098c3aa3537ea8626b0bd4708eb2e7e9fed30f
qyd <saviorbruce@gmail.com>
2018-12-15 16:32:43 -0800

    implement job Delete button for canis analysis

--- src/ui/components/JobDetails/JobDetails.jsx
+++ src/ui/components/JobDetails/JobDetails.jsx
@@ -4,6 +4,8 @@ import * as React from 'react';
 import * as PropTypes from 'prop-types';
 import Collapsible from '../Shared/Collapsible/Collapsible';
 import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
+import Dialog from 'material-ui/Dialog';
 import NavigationRefresh from 'material-ui/svg-icons/navigation/refresh';
 import JobResultDialog from '../JobResultDialog/JobResultDialog';
 import { Canis } from 'valis';
@@ -15,15 +17,16 @@ class JobDetails extends React.Component {
     this.state = {
       job: null,
       showResultDialog: false,
+      showConfirmDeleteDialog: false,
     };
   }
 
-	componentDidMount() {
+  componentDidMount() {
     this.refresh();
   }
 
   refresh = () => {
-    Canis.Api.getJob(this.props.job.id).then(d=> {
+    Canis.Api.getJob(this.props.job.id).then(d => {
       this.setState({
         job: d,
       })
@@ -42,6 +45,26 @@ class JobDetails extends React.Component {
     })
   }
 
+  handleOpenConfirmDialog = () => {
+    this.setState({
+      showConfirmDeleteDialog: true,
+    })
+  }
+
+  handleCloseConfirmDialog = () => {
+    this.setState({
+      showConfirmDeleteDialog: false,
+    })
+  }
+
+  handleConfirmDelete = () => {
+    Canis.Api.deleteJob(this.props.job.id);
+    this.setState({
+      showConfirmDeleteDialog: false,
+    })
+    this.appModel.popView();
+  }
+
   render() {
     let jobInfo = 'Loading job status...';
     let link = null;
@@ -49,12 +72,12 @@ class JobDetails extends React.Component {
     if (this.state.job) {
       jobInfo = (<span>
         Status: <b>{this.state.job.status}</b>
-        <br/>
-        {this.state.job.result ? (<span>Result: <b>{this.state.job.result}</b></span>) : (<FlatButton onClick={this.refresh} style={{color: 'white'}} label="Refresh Status" icon={(<NavigationRefresh/>)} />) }
+        <br />
+        {this.state.job.result ? (<span>Result: <b>{this.state.job.result}</b></span>) : (<FlatButton onClick={this.refresh} style={{ color: 'white' }} label="Refresh Status" icon={(<NavigationRefresh />)} />)}
       </span>)
 
       if (this.state.job.status === 'DONE') {
-        link = (<Collapsible onClick={this.handleOpenDialog} title={'View results'}  isLink={true}/>);
+        link = (<Collapsible onClick={this.handleOpenDialog} title={'View results'} isLink={true} />);
         resultDialog = <JobResultDialog job={this.state.job} open={this.state.showResultDialog} handleClose={this.handleCloseDialog} />;
       }
     }
@@ -62,14 +85,35 @@ class JobDetails extends React.Component {
       <div className="sidebar-name">{this.props.job.name}</div>
       <div className="sidebar-description">{jobInfo}</div>
     </div>);
-
+    const deleteButton = <RaisedButton
+      label="Delete"
+      secondary={true}
+      onClick={this.handleOpenConfirmDialog}
+      style={{ position: "absolute", bottom: "10px", width: "90%", marginLeft: "5%" }}
+    />
+    const confirmDialogActions = [
+      <FlatButton onClick={this.handleCloseConfirmDialog} primary autoFocus>
+        Cancel
+      </FlatButton>,
+      <FlatButton onClick={this.handleConfirmDelete} secondary>
+        Confirm
+      </FlatButton>
+    ];
+    const confirmDeleteDialog = <Dialog
+      open={this.state.showConfirmDeleteDialog}
+      onClose={this.handleCloseConfirmDialog}
+      title={"Delete this analysis?"}
+      actions={confirmDialogActions}
+    >
+      Analysis deleted can not be recovered after this.
+    </Dialog>
     return (
-      <div>
-        <div className="job-details">
-          {header}
-          {link}
-        </div>
+      <div className="job-details">
+        {header}
+        {link}
+        {deleteButton}
         {resultDialog}
+        {confirmDeleteDialog}
       </div>
     );
   }
